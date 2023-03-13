b827825c0dfb8c8fbf229bda3f33b976a45bb8df
Saida Mahmood <saida.mahmood04@gmail.com>
2023-03-13 22:49:27 +0800

    Projrct details added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path='/'  element={<Dashboard />} />
+          <Route exact path='/' element={<Dashboard />} />
+             <Route path='/project/:id'  element={<ProjectDetails/>} />
         </Routes>
    
       </div>
